bfd59c7e387ca00bde075e2bfb715cbf58e6b746
Giovanna <Gi@email.com>
2025-02-26 11:05:22 -0300

    Término da Fase 3 - Implementado endpoint FindByCityIgnoreCase. -Update AirportRepository - Adicionado assinatura de método findByCityIgnoreCase;
    -Update AirportService - Adicionado método findByCity
    -Update AirportController - Adicionado endpoint /city/{cityName}

--- src/main/java/local/gih/airports/controllers/AirportController.java
+++ src/main/java/local/gih/airports/controllers/AirportController.java
@@ -4,7 +4,9 @@ import java.util.List;
 import local.gih.airports.entities.Airport;
 import local.gih.airports.services.AirportService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -23,4 +25,17 @@ public class AirportController {
         return result;
     }
     
+    @GetMapping("/city/{cityName}")
+    public ResponseEntity<List<Airport>> findByCityIgnoreCase(@PathVariable String cityName){
+        List<Airport> result = airportService.findByCity(cityName);
+        
+        if (result.isEmpty()){
+            return ResponseEntity.notFound().build();
+            
+        }else{ 
+            
+            return ResponseEntity.ok(result);
+        }
+      
+    }
 }
--- src/main/java/local/gih/airports/repositories/AirportRepository.java
+++ src/main/java/local/gih/airports/repositories/AirportRepository.java
@@ -1,5 +1,6 @@
 package local.gih.airports.repositories;
 
+import java.util.List;
 import local.gih.airports.entities.Airport;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,6 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @author sesideva
  */
 public interface AirportRepository extends JpaRepository<Airport, Long> {
-
     
+    List<Airport> findByCityIgnoreCase(String city);
+
 }
